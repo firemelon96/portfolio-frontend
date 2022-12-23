ae88797a16c691f28c2eebde8b9b7cada9041c14
estong jamion <estong.jamion@gmail.com>
2022-12-23 10:39:32 +0300

    created style for the 404 not found

--- src/components/errorPage.js
+++ src/components/errorPage.js
@@ -3,8 +3,11 @@ import React from "react";
 function ErrorPage() {
   return (
     <div>
-      <div className="">
-        <h1>Page Not found</h1>
+      <div className="h-screen flex items-center justify-center bg-primary">
+        <div className="flex gap-4 text-6xl text-semibold">
+          <h1 className="text-tertiary">404</h1>
+          <h1 className="text-fourth">Page Not found</h1>
+        </div>
       </div>
     </div>
   );
